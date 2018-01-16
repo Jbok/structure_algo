a4acd32a6203179b6e72190cd74bf1a9d34aa445
Jbok <0614bok@gmail.com>
2018-01-17 02:03:15 +0900

    Polynomial v0.1
    -separate cpp file and header file according to useage.

---
+++ LinkedList/linkedlist_func.h
@@ -0,0 +1,26 @@
+#ifndef __LINKEDLIST_FUNC_H__
+#define __LINKEDLIST_FUNC_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct node {
+	char *data;
+	node *next;
+};
+
+typedef struct node Node;
+
+void add_head(char *name, Node **head);
+int add_after(char *item, Node *prev);
+Node *remove_head(Node **ptr_head);
+Node *remove_after(Node *prev);
+Node *find(char *item, Node *head);
+int find_index(char *item, Node *head);
+Node *get_node(int index, Node *head);
+int add(int index, char *item, Node *head);
+Node *remove(int index, Node *head);
+Node *remove(char *item, Node *head);
+
+#endif
---
+++ LinkedList/polynomial.h
@@ -0,0 +1,7 @@
+#ifndef __POLYNOMIAL_H__
+#define __POLYNOMIAL_H__
+
+#include "linkedlist_func.h"
+
+
+#endif
